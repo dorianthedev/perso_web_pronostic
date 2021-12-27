8baa52a63690bbc27e468206fbdfb8c4c5b2128d
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2021-12-27 14:55:27 +0100

    add correctif

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const header = document.querySelector('header');
-const burgerClos = document.querySelector('.burger-close');
+const burgerClose = document.querySelector('.burger-close');
 
 
 burgerMenu.addEventListener("click", () => {
     header.classList.add('ouvrir')
-})
+});
 
-burgerClos.addEventListener("click", () => {
+burgerClose.addEventListener("click", () => {
     header.classList.remove('ouvrir')
-})
+});
