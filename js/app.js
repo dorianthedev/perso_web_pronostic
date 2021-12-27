1370e153ec17a7f5225cc357cec555f71edbf60a
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2021-12-27 10:45:47 +0100

    add js menu burger

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const header = document.querySelector('header');
+const burgerClose = document.querySelector('.burger-close');
+
+
+burgerMenu.addEventListener("click", () => {
+    header.classList.add('ouvrir')
+})
+
+burgerClose.addEventListener("click", () => {
+    header.classList.remove('ouvrir')
+})
